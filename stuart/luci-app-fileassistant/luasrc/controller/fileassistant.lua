8ca0a3080c7c6c104624fefbd6be9a758e25f292
Stuart Hua <stuarthua.cn@gmail.com>
2019-09-13 23:51:07 +0800

    repost

--- stuart/luci-app-fileassistant/luasrc/controller/fileassistant.lua
+++ stuart/luci-app-fileassistant/luasrc/controller/fileassistant.lua
@@ -86,7 +86,7 @@ end
 function fileassistant_install()
     local filepath = luci.http.formvalue("filepath")
     local success = os.execute('opke install "'..filepath..'"')
-    list_response(filepath, success)
+    list_response(nixio.fs.dirname(filepath), success)
 end
 
 function fileassistant_upload()
