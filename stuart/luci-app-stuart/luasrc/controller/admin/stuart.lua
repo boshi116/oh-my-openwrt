4c88d32e841b563e8b089944af8cdfb8c310e9a3
Stuart Hua <stuarthua.cn@gmail.com>
2019-09-13 21:24:09 +0800

    repost

--- stuart/luci-app-stuart/luasrc/controller/admin/stuart.lua
+++ stuart/luci-app-stuart/luasrc/controller/admin/stuart.lua
@@ -2,5 +2,5 @@ module("luci.controller.admin.stuart",package.seeall)
 
 function index()
 	entry({"admin", "stuart"}, firstchild(), _("Stuart"), 89).index = true
-	entry({"admin", "stuart", "helloworld"}, template("stuart/helloworld"), "HelloWorld", 1)
+	entry({"admin", "stuart", "helloworld"}, template("helloworld"), "HelloWorld", 1)
 end
