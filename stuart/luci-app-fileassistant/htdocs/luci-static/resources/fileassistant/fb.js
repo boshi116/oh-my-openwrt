46a2c0b895142e84fb771e02917a896cfe8c8026
Stuart Hua <stuarthua.cn@gmail.com>
2019-09-13 21:47:35 +0800

    repost

--- stuart/luci-app-fileassistant/htdocs/luci-static/resources/fileassistant/fb.js
+++ stuart/luci-app-fileassistant/htdocs/luci-static/resources/fileassistant/fb.js
@@ -19,7 +19,7 @@ String.prototype.replaceAll = function(search, replacement) {
   function removePath(filename, isdir) {
     var c = confirm('你确定要删除 ' + filename + ' 吗？');
     if (c) {
-      iwxhr.get('/cgi-bin/luci/admin/nas/filebrowser_mini_delete',
+      iwxhr.get('/cgi-bin/luci/admin/stuart/fileassistant_delete',
         {
           path: concatPath(currentPath, filename),
           isdir: isdir
@@ -37,7 +37,7 @@ String.prototype.replaceAll = function(search, replacement) {
       newname = newname.trim();
       if (newname != filename) {
         var newpath = concatPath(currentPath, newname);
-        iwxhr.get('/cgi-bin/luci/admin/nas/filebrowser_mini_rename',
+        iwxhr.get('/cgi-bin/luci/admin/stuart/fileassistant_rename',
           {
             filepath: concatPath(currentPath, filename),
             newpath: newpath
@@ -54,7 +54,7 @@ String.prototype.replaceAll = function(search, replacement) {
 
   function openpath(filename, dirname) {
     dirname = dirname || currentPath;
-    window.open('/cgi-bin/luci/admin/nas/filebrowser_mini_open?path='
+    window.open('/cgi-bin/luci/admin/stuart/fileassistant_open?path='
       + encodeURIComponent(dirname) + '&filename='
       + encodeURIComponent(filename));
   }
@@ -156,7 +156,7 @@ String.prototype.replaceAll = function(search, replacement) {
     opt = opt || {};
     path = concatPath(path, '');
     if (currentPath != path) {
-      iwxhr.get('/cgi-bin/luci/admin/nas/filebrowser_mini_list',
+      iwxhr.get('/cgi-bin/luci/admin/stuart/fileassistant_list',
         {path: path},
         function (x, res) {
           if (res.ec === 0) {
@@ -201,7 +201,7 @@ String.prototype.replaceAll = function(search, replacement) {
       formData.append('upload-dir', concatPath(currentPath, ''));
       formData.append('upload-file', uploadinput.files[0]);
       var xhr = new XMLHttpRequest();
-      xhr.open("POST", "/cgi-bin/luci/admin/nas/filebrowser_mini_upload", true);
+      xhr.open("POST", "/cgi-bin/luci/admin/stuart/fileassistant_upload", true);
       xhr.onload = function() {
         if (xhr.status == 200) {
           var res = JSON.parse(xhr.responseText);
