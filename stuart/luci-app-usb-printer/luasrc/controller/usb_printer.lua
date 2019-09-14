d0281bae5905bf9933fbfcc48df741f8c7475b43
Stuart Hua <stuarthua.cn@gmail.com>
2019-09-14 16:30:07 +0800

    repost

---
+++ stuart/luci-app-usb-printer/luasrc/controller/usb_printer.lua
@@ -0,0 +1,29 @@
+--[[
+LuCI - Lua Configuration Interface
+
+Copyright 2008 Steven Barth <steven@midlink.org>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+$Id$
+]]--
+
+require("luci.sys")
+
+module("luci.controller.usb_printer", package.seeall)
+
+function index()
+	if not nixio.fs.access("/etc/config/usb_printer") then
+		return
+	end
+	
+	entry({"admin", "stuart"}, firstchild(), "Stuart", 89).dependent = false
+	
+	page = entry({"admin", "stuart", "usb_printer"}, cbi("usb_printer"), _("USB Printer Server"), 3)
+	page.i18n = "base"
+    page.dependent = true
+end
