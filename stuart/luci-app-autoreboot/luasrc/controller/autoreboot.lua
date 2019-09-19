4a33ff60ce08934b2b13edfee6bd8cca47c36bc8
Stuart Hua <stuarthua.cn@gmail.com>
2019-09-19 22:26:53 +0800

    repost

--- stuart/luci-app-autoreboot/luasrc/controller/autoreboot.lua
+++ stuart/luci-app-autoreboot/luasrc/controller/autoreboot.lua
@@ -1,9 +1,5 @@
 module("luci.controller.autoreboot",package.seeall)
-
 function index()
     entry({"admin", "custom"}, firstchild(), "我的", 89).dependent = false
-	local page
-	page = entry({"admin", "custom", "autoreboot"}, cbi("autoreboot"), _("Scheduled Reboot"), 997)
-	page.i18n = "autoreboot"
-    page.dependent = true
+    entry({"admin", "custom", "autoreboot"}, cbi("autoreboot"), _("Scheduled Reboot"), 997)
 end
