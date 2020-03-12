002cdc2b442ce4d62c5c3604bc7f29a422ceb866
stuarthua <stuarthua.cn@gmail.com>
2020-03-12 23:46:44 +0800

    repost by shell.

--- README.md
+++ README.md
@@ -2,39 +2,15 @@
 
 ---
 
-Using OpenWrt Experience
-
 Reading: [gh-pages](https://stuarthua.github.io/oh-my-openwrt/)
 
 ## Usage
 
-### Make Image or Packages
-
-Edit `feeds.conf.default`, add below line
-
-```
-src-git stuart https://github.com/stuarthua/oh-my-openwrt
-```
-
-Execute
-
 ```bash
-$ ./scripts/feeds update -a
-$ ./scripts/feeds install -a
+git clone https://github.com/stuarthua/oh-my-openwrt
+cd oh-my-openwrt
+./start.sh
 ```
-
-Make and Enjoy.
-
-### Use My IPK Files
-
-checkout devices
-
-```bash
-$ git clone https://github.com/stuarthua/oh-my-openwrt
-$ git checkout -b devices origin/devices
-```
-
-Use Image Builder to gen your image or just install it on your devices.
 
 ## Thanks
 
--- start.sh
+++ start.sh
@@ -7,3 +7,24 @@ INPUT="$BOLD => $NORM"
 ERROR="\033[31m *** Error: $NORM"
 WARNING="\033[33m * Warning: $NORM"
 
+# tips
+echo -e "$INFO 1. 编译 lean openwrt"
+echo -e "$INFO 2. 编译 stuart openwrt for xiaomi"
+echo -e "$INFO 3. 编译 stuart openwrt for newifi3"
+echo -e "$INFO 4. 编译 stuart openwrt for x86_64"
+echo -e "$INFO 0. 索引 ipks 并签名"
+
+while true; do
+    echo -n -e "$INPUT"
+    read -s -p "请输入操作序号 (0-4)" yn
+    echo
+    case $yn in
+        1 ) bash scripts/build-lean.sh; break;;
+        2 ) bash scripts/xiaomi.sh; break;;
+        3 ) bash scripts/newifi3.sh; break;;
+        4 ) bash scripts/x86_64.sh; break;;
+        0 ) bash scripts/index-ipks.sh; exit;;
+        "") echo -e "$INFO end!"; exit;;
+        * ) echo "输入 0-4 以确认";;
+    esac
+done
