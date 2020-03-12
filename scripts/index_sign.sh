34ecb110e25bde210bd4ed667460dad51c8ce9b7
stuarthua <stuarthua.cn@gmail.com>
2020-03-13 05:03:46 +0800

    repost by shell.

--- scripts/index_sign.sh
+++ scripts/index_sign.sh
@@ -10,29 +10,90 @@ WARNING="\033[33m * Warning: $NORM"
 # if error occured, then exit
 set -e
 
+# prepare
+if [ ! -d build_openwrt ]; then
+    mkdir -p build_openwrt
+fi
+cd build_openwrt
+
 # path
 root_path=`pwd`
-project_path="$root_path/lean-openwrt"
+project="lean-openwrt"
+code_path="$root_path/lean-openwrt"
 # 1 小米路由器青春版, 2 Newifi3, 3 软路由
 device_type=3
 # 1 lean, 2 18.06.8, 3 19.07.2
 index_type=1
 
-pre_project(){
-
+######################## download app code from lean openwrt rep ########################
+do_update_code(){
+    echo "update code..."
+    cd $code_path
+    git pull origin master 1>/dev/null 2>&1
+    echo -e "$INFO code update done!"
+}
+do_clone_code(){
+    echo "clone code..."
+    cd $root_path
+    rm -rf $code_path
+    git clone https://github.com/coolsnowwolf/lede.git $project
+    echo -e "$INFO code clone done!"
+}
+clone_or_update_code(){
+    if [ ! -d $code_path ]; then
+        mkdir -p $code_path
+    fi
+    result=`ls $code_path`
+    if [ -z "$result" ]; then
+        do_clone_code
+    else
+        do_update_code
+    fi
 }
 
+clone_or_update_code
+
+######################## feeds update and install ########################
+# 修复 18.04 动态链接库缺失问题
+fix_sys(){
+    if [ ! -L /lib/ld-linux-x86-64.so.2 ]; then
+        sudo ln -s /lib/x86_64-linux-gnu/ld-2.27.so /lib/ld-linux-x86-64.so.2
+    fi
+}
+fix_sys
+# feeds download
+do_update_feeds(){
+    echo "download feeds begin..."
+    cd $code_path
+    ./scripts/feeds update -a && ./scripts/feeds install -a
+    echo -e "$INFO download feeds done!"
+}
+update_feeds(){
+    while true; do
+        echo -n -e "$INPUT"
+        read -p "是否 安装/更新 feeds (y/n) ?" yn
+        echo
+        case $yn in
+            [Yy]* ) do_update_feeds; break;;
+            [Nn]* | "" ) break;;
+            * ) echo "输入 y 或 n 以确认";;
+        esac
+    done
+}
+update_feeds
+
+######################## index and sign ########################
 gen_device_desc(){
     if [ $device_type -eq 1 ]; then
-        device_desc="xiaomi"
+        device="xiaomi"
         device_ipk_desc="mipsel_24kc"
         device_bin_desc="ramips/mt76x8"
     elif [ $device_type -eq 2 ]; then
-        device_desc="newifi3"
+        device="newifi3"
         device_ipk_desc="newifi3"
         device_bin_desc="ramips/newifi3"
     elif [ $device_type -eq 3 ]; then
-        device_desc="x86_64"
+        device="x86_64"
         device_ipk_desc="x86_64"
         device_bin_desc="x86/x86_64"
     else
@@ -45,20 +106,20 @@ gen_index(){
     tmp_dir=$1
     cd $tmp_dir
     rm -f Packages*
-    $project_path/scripts/ipkg-make-index.sh . 2>/dev/null > Packages.manifest
+    $code_path/scripts/ipkg-make-index.sh . 2>/dev/null > Packages.manifest
     grep -vE '^(Maintainer|LicenseFiles|Source|Require)' Packages.manifest > Packages
     gzip -9nc Packages > Packages.gz
-    $project_path/staging_dir/host/bin/usign -S -m Packages -s $root_path/openwrt-stuart.key
+    $code_path/staging_dir/host/bin/usign -S -m Packages -s $root_path/openwrt-stuart.key
 }
 
 index_ipk(){
     echo "gen ipks index begin..."
 
     tmp_env=$PATH
-    export PATH="$project_path/staging_dir/host/bin:$PATH"
+    export PATH="$code_path/staging_dir/host/bin:$PATH"
 
     gen_index "$1/luci"
-    gen_index "$1/base/$device_desc"
+    gen_index "$1/base/$device"
 
     unset PATH
     export PATH="$tmp_env"
@@ -78,14 +139,14 @@ dir_index_ipk(){
         exit
     fi
 
-    artifact_ipk_path="$artifact_root_path/packages/$device_desc"
+    artifact_ipk_path="$artifact_root_path/packages/$device"
     index_ipk "$artifact_ipk_path"
 }
 
 # gen key
 if [ ! -e $root_path/openwrt-stuart.key ]; then
     echo "openwrt-stuart.key gen..."
-    $project_path/staging_dir/host/bin/usign -G -p $root_path/openwrt-stuart.pub -s $root_path/openwrt-stuart.key
+    $code_path/staging_dir/host/bin/usign -G -p $root_path/openwrt-stuart.pub -s $root_path/openwrt-stuart.key
     echo -e "$INFO openwrt-stuart.key gen done!"
 fi
 
--- scripts/lean.sh
+++ scripts/lean.sh
@@ -18,15 +18,15 @@ device_type=3
 
 gen_device_desc(){
     if [ $device_type -eq 1 ]; then
-        device_desc="xiaomi"
+        device="xiaomi"
         device_ipk_desc="mipsel_24kc"
         device_bin_desc="ramips/mt76x8"
     elif [ $device_type -eq 2 ]; then
-        device_desc="newifi3"
+        device="newifi3"
         device_ipk_desc="newifi3"
         device_bin_desc="ramips/newifi3"
     elif [ $device_type -eq 3 ]; then
-        device_desc="x86_64"
+        device="x86_64"
         device_ipk_desc="x86_64"
         device_bin_desc="x86/x86_64"
     else
@@ -47,98 +47,92 @@ while true; do
     esac
 done
 
+# prepare
+if [ ! -d build_openwrt ]; then
+    mkdir -p build_openwrt
+fi
+cd build_openwrt
+
 # path
 root_path=`pwd`
 project_path="$root_path/$project"
-ipk_path="$project_path/bin/packages/$device_ipk_desc"
-bin_path="$project_path/bin/targets/$device_bin_desc"
-artifact_root_path="$root_path/lean"
-artifact_bin_path="$artifact_root_path/targets/$device_desc"
-artifact_ipk_path="$artifact_root_path/packages/$device_desc"
+ipk_path="$code_path/bin/packages/$device_ipk_desc"
+bin_path="$code_path/bin/targets/$device_bin_desc"
+artifact_root_path="$root_path/artifacts/lean"
+artifact_bin_path="$artifact_root_path/targets/$device"
+artifact_ipk_path="$artifact_root_path/packages/$device"
 
+######################## set env ########################
 # dir for project and artifact
-if [ ! -d $project_path ]; then
-    mkdir -p $project_path
-fi
-if [ ! -d $bin_path ]; then
-    mkdir -p $bin_path
-fi
-if [ ! -d $ipk_path/stuart ]; then
-    mkdir -p $ipk_path/stuart
-fi
-if [ ! -d $artifact_root_path ]; then
-    mkdir -p $artifact_root_path
-fi
-if [ ! -d $artifact_bin_path ]; then
-    mkdir -p $artifact_bin_path
-fi
-if [ ! -d $artifact_ipk_path ]; then
-    mkdir -p $artifact_ipk_path
-fi
-if [ ! -d $artifact_ipk_path/luci ]; then
-    mkdir -p $artifact_ipk_path/luci
-fi
-if [ ! -d $artifact_ipk_path/base ]; then
-    mkdir -p $artifact_ipk_path/base
-fi
-echo -e "$INFO artifact dir set done!"
+pre_artifacts_dir(){
+    if [ ! -d $code_path ]; then
+        mkdir -p $code_path
+    fi
+    if [ ! -d $bin_path ]; then
+        mkdir -p $bin_path
+    fi
+    if [ ! -d $ipk_path/stuart ]; then
+        mkdir -p $ipk_path/stuart
+    fi
+    if [ ! -d $artifact_root_path ]; then
+        mkdir -p $artifact_root_path
+    fi
+    if [ ! -d $artifact_bin_path ]; then
+        mkdir -p $artifact_bin_path
+    fi
+    if [ ! -d $artifact_ipk_path ]; then
+        mkdir -p $artifact_ipk_path
+    fi
+    if [ ! -d $artifact_ipk_path/luci ]; then
+        mkdir -p $artifact_ipk_path/luci
+    fi
+    if [ ! -d $artifact_ipk_path/base ]; then
+        mkdir -p $artifact_ipk_path/base
+    fi
+    echo -e "$INFO artifact dir set done!"
+}
+pre_artifacts_dir
 
 ######################## download app code from lean openwrt rep ########################
 do_update_code(){
     echo "update code..."
-    cd $project_path
+    cd $code_path
     git pull origin master 1>/dev/null 2>&1
     echo -e "$INFO code update done!"
 }
-update_code(){
-    while true; do
-        echo -n -e "$INPUT"
-        read -p "是否更新 lean openwrt 仓库代码 (y/n) ?" yn
-        echo
-        case $yn in
-            [Yy]* ) do_update_code; break;;
-            [Nn]* | "" ) break;;
-            * ) echo "输入 y 或 n 以确认";;
-        esac
-    done
-}
-
-result=`ls $project_path`
-if [ -z "$result" ]; then
+do_clone_code(){
+    echo "clone code..."
     cd $root_path
-    git clone https://github.com/coolsnowwolf/lede.git lean-openwrt
-    echo -e "$INFO code download done!"
-else
-    update_code
-fi
-
-# install build dependency
-## @https://github.com/coolsnowwolf/lede
-## @https://github.com/esirplayground/lede
-do_install_dep(){
-    echo "install build dependency begin..."
-    sudo apt update
-    sudo apt-get -y install build-essential asciidoc binutils bzip2 gawk gettext git libncurses5-dev patch python3.5 unzip zlib1g-dev lib32gcc1 libc6-dev-i386 subversion flex node-uglify gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libglib2.0-dev xmlto qemu-utils upx-ucl libelf-dev autoconf automake libtool autopoint device-tree-compiler libuv-dev g++-multilib linux-libc-dev
-    echo -e "$INFO install build dependency done!"
+    rm -rf $code_path
+    git clone https://github.com/coolsnowwolf/lede.git $project
+    echo -e "$INFO code clone done!"
 }
-install_dep(){
-    while true; do
-        echo -n -e "$INPUT"
-        read -p "是否 安装/更新 软件包编译依赖 (y/n) ?" yn
-        echo
-        case $yn in
-            [Yy]* ) do_install_dep; break;;
-            [Nn]* | "" ) break;;
-            * ) echo "输入 y 或 n 以确认";;
-        esac
-    done
+clone_or_update_code(){
+    if [ ! -d $code_path ]; then
+        mkdir -p $code_path
+    fi
+    result=`ls $code_path`
+    if [ -z "$result" ]; then
+        do_clone_code
+    else
+        do_update_code
+    fi
 }
-install_dep
 
+clone_or_update_code
+
+######################## feeds update and install ########################
+# 修复 18.04 动态链接库缺失问题
+fix_sys(){
+    if [ ! -L /lib/ld-linux-x86-64.so.2 ]; then
+        sudo ln -s /lib/x86_64-linux-gnu/ld-2.27.so /lib/ld-linux-x86-64.so.2
+    fi
+}
+fix_sys
 # feeds download
 do_update_feeds(){
     echo "download feeds begin..."
-    cd $project_path
+    cd $code_path
     ./scripts/feeds update -a && ./scripts/feeds install -a
     echo -e "$INFO download feeds done!"
 }
@@ -156,14 +150,6 @@ update_feeds(){
 }
 update_feeds
 
-# 修复 18.04 动态链接库缺失问题
-fix_sys(){
-    if [ ! -L /lib/ld-linux-x86-64.so.2 ]; then
-        sudo ln -s /lib/x86_64-linux-gnu/ld-2.27.so /lib/ld-linux-x86-64.so.2
-    fi
-}
-fix_sys
-
 ######################## build openwrt ########################
 archive_bin(){
     cd $bin_path
@@ -171,7 +157,7 @@ archive_bin(){
 }
 do_build_openwrt(){
     echo "build begin..."
-    cd $project_path
+    cd $code_path
     make menuconfig
     make download
     make V=s
@@ -210,9 +196,9 @@ archive_ssr_ipk(){
 
 do_build_ssr_ipk(){
     echo "build ssr begin..."
-    cd $root_path/lean-openwrt
+    cd $code_path
     make menuconfig
-    # make target/linux/compile V=s
+    make target/linux/compile V=s
     make package/lean/luci-app-ssr-plus/compile V=s
     echo -e "$INFO build ssr done!"
 
