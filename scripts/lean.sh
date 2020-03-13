eaaf36ce6287bfe1fc620e1cf829346e4e5d77e8
stuarthua <stuarthua.cn@gmail.com>
2020-03-13 12:18:52 +0800

    repost by shell.

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -249,7 +249,7 @@ archive_ssr_ipk(){
     cd $ipk_path/base
     cp -f luci-app-ssr-plus*_all.ipk $artifact_ipk_path/luci/
     # dependency
-    cp -f shadowsocksr-libev-*$device_ipk_desc.ipk $artifact_ipk_path/base/$device/
+    sudo cp -f shadowsocksr-libev-*$device_ipk_desc.ipk $artifact_ipk_path/base/$device/
     cp -f libopenssl*$device_ipk_desc.ipk $artifact_ipk_path/base/$device/
     cp -f ipt2socks*$device_ipk_desc.ipk $artifact_ipk_path/base/$device/
     cp -f microsocks*$device_ipk_desc.ipk $artifact_ipk_path/base/$device/
